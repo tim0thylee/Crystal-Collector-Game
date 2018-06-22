cc9a8b3ae13dce00a8ffae98151ab84b58f2aaff
Timothy Lee <timothylee43@gmail.com>
2018-06-21 22:34:42 -0700

    fixed initial random number now showing

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# week-4-game
+# Crystal Collector Game
+
+* A random number will be chosen at the start of the game. 
+* Four crystals are displayed on the page, each with a hidden random value.
+* When a crystal is clicked on, it will be added to the player's total score.
+* If the player's total score matches the initial random number, the player wins!
+* If the player's total score exceeds teh intial random number, the players loses.
+* Total wins and losses will be tracked.
+
+This game was created using jQuery and HTML5. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,77 @@
-var wins = 0;
-var losses = 0;
-var addition = 0;
-
-var total = Math.round((Math.random()* 100) + 19);
-
-var red = Math.round((Math.random()* 12) + 1);
-var blue = Math.round((Math.random()* 12) + 1);
-var yellow = Math.round((Math.random()* 12) + 1);
-var green = Math.round((Math.random()* 12) + 1);
-
-
-var totalSum = function (){
-	$('.sum').empty();
-	$('.sum').append(addition);
-
-	$('#wins').empty();
-	$('#wins').append(wins);
-
-	$('#losses').empty();
-	$('#losses').append(losses);
-}
-
-var restart = function(){
-	addition = 0;
-    total = Math.round((Math.random()* 100) + 19);
-
-	$('.total').empty();
-	$('.total').append(total);
-
-	red = Math.round((Math.random()* 12) + 1);
-    blue = Math.round((Math.random()* 12) + 1);
-    yellow = Math.round((Math.random()* 12) + 1);
-    green = Math.round((Math.random()* 12) + 1);
-   
-    totalSum();
-}
-
-var game = function (){
-	if (addition === total) {
-		wins = wins + 1;
+$(document).ready(function(){
+	
+	var wins = 0;
+	var losses = 0;
+	var addition = 0;
+	
+	var total = Math.round((Math.random()* 100) + 19);
+	
+	var red = Math.round((Math.random()* 12) + 1);
+	var blue = Math.round((Math.random()* 12) + 1);
+	var yellow = Math.round((Math.random()* 12) + 1);
+	var green = Math.round((Math.random()* 12) + 1);
+	
+	
+	var totalSum = function (){
+		$('.sum').empty();
+		$('.sum').append(addition);
 		
-		restart();
-	}
-
-	else if(addition > total){
-		losses = losses + 1;
+		$('#wins').empty();
+		$('#wins').append(wins);
 		
-		restart();
+		$('#losses').empty();
+		$('#losses').append(losses);
 	}
-
-	else{
+	
+	var restart = function(){
+		addition = 0;
+		total = Math.round((Math.random()* 100) + 19);
+		
+		$('.total').empty();
+		$('.total').append(total);
+		
+		red = Math.round((Math.random()* 12) + 1);
+		blue = Math.round((Math.random()* 12) + 1);
+		yellow = Math.round((Math.random()* 12) + 1);
+		green = Math.round((Math.random()* 12) + 1);
+		
 		totalSum();
 	}
-}
-
-$('.total').append(total);
-
-$('.sum').append(addition);
-
-
-$(document).ready(function(){
+	
+	var game = function (){
+		if (addition === total) {
+			wins = wins + 1;
+			
+			restart();
+		}
+		
+		else if(addition > total){
+			losses = losses + 1;
+			
+			restart();
+		}
+		
+		else{
+			totalSum();
+		}
+	}
+	
+	$('.total').append(total);
+	
+	$('.sum').append(addition);
+	
+	
+	
 	$('#red').click(function(){
 		addition = addition + red;
 		game();
 	})
-
+	
 	$('#blue').click(function(){
 		addition = addition + blue;
 		game();
 	})
-
+	
 	$('#yellow').click(function(){
 		addition = addition + yellow;
 		game();
@@ -80,6 +82,7 @@ $(document).ready(function(){
 		game();
 	})
 });
+
 
 
 
